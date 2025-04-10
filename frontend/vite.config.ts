54a88a89f72e393a0abaca0ac1d3feb0b5acf764
7ty2ryz3 <iasakov1978@gmail.com>
2025-04-10 20:29:25 +0500

    Added docker integration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:5000',
+    }
+  },
   root: "src",
   plugins: [react()],
 })
